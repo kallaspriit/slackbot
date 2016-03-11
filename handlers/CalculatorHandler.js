30da1b66e87a4ea7eeaf7cc6ec65b87847d54db6
Priit Kallas <kallaspriit@gmail.com>
2016-03-11 16:07:54 +0200

    Improved calculator.

--- handlers/CalculatorHandler.js
+++ handlers/CalculatorHandler.js
@@ -12,10 +12,9 @@ export default class CalculatorHandler extends BaseHandler {
 
 	match(message) {
 		const mathExpressionRegex = /^[0-9\+\-\*\/\^\(\)\.]{3,}$/;
-		const allNumbersRegex = /^[0-9\.]+$/;
 
-		// validate that only constains match expressions characters but is not all just numbers
-		if (!mathExpressionRegex.test(message.text) || allNumbersRegex.test(message.text)) {
+		// validate that only constains math expressions characters
+		if (!mathExpressionRegex.test(message.text)) {
 			return false;
 		}
 
@@ -32,11 +31,12 @@ export default class CalculatorHandler extends BaseHandler {
 		try {
 			const result = eval(message.text); // eslint-disable-line no-eval
 
-			if (typeof result === 'number') {
-				message.respond(message.text + '=*' + result + '*');
-			} else {
-				message.respond(result);
+			// don't respond if calculated response is the same as input
+			if (message.text === result || typeof result !== 'number' || Number.isNaN(result)) {
+				return;
 			}
+
+			message.respond(message.text + '=*' + result + '*');
 		} catch (e) {
 			// ignore
 		}
