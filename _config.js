030f8a0f2c1f42e166ea90e28d1ffea4c3220375
Priit Kallas <kallaspriit@gmail.com>
2016-07-05 13:36:20 +0300

    Added configuration example for facebook.

--- _config.js
+++ _config.js
@@ -1,5 +1,9 @@
 export default {
 	token: '',
 	name: 'SlackBot',
-	picture: 'http://www.gatewayapps.com/wp-content/uploads/2015/02/slackLogo1.png'
+	picture: 'http://www.gatewayapps.com/wp-content/uploads/2015/02/slackLogo1.png',
+	facebook: {
+		id: 'add from https://developers.facebook.com/apps/',
+		secret: 'xxx'
+	}
 };
